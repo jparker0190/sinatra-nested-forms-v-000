13364ed1c89df42cad419a90c32b6e3930e5efed
Joseph Parker <jparker010190@gmail.com>
2018-05-27 16:18:51 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,16 +1,16 @@
 class Ship
-  @@all = []
-  attr_accessor :name, :type, :booty
-  def initialize(name, type, booty)
-    @name = name
-    @type = type
-    @booty = booty
-    @@all << self
+  SHIPS = []
+  attr_reader :name, :type, :booty
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIPS << self
   end
   def self.all
-    @@all
+    SHIPS
   end
   def self.clear
-    @@all.clear
+    SHIPS.clear
   end
 end
